c7b10ccdb22729f6b5f7efbd92db44371d1c1a5b
) <dansteel53@gmail.com>
2022-05-26 02:58:44 +0300

    Update check_stats.py

--- check_stats.py
+++ check_stats.py
@@ -1,7 +1,6 @@
 import psutil
 import os
-
-print('RAM used:', psutil.virtual_memory()[2], '%')
+import json
 
 total_memory, used_memory, free_memory = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
 
@@ -13,7 +12,7 @@ hdd = psutil.disk_usage('/')
 print("vaba salvestusruumi:", round(hdd.free / (10**9), 2), "gb")
 print("kokku salvestusruumi:", round(hdd.total / (10**9), 2), "gb")
 
-warning = 50
+warning = 20
 
 if round((used_memory/total_memory) * 100, 2) > warning:
     print("ram usage is more than", warning, "%")
@@ -24,4 +23,24 @@ if round((hdd.used/hdd.free) * 100, 2) > warning:
     print("disk usage is more than", warning, "%")
 else:
     print("disk usage is fine")
+
+pc_ram = {"ram":{
+    "free ram in gb": round(free_memory/1000, 2),
+    "total ram in gb": round(total_memory/1000, 2),
+    "used ram in %": round((used_memory/total_memory) * 100, 2)
+    }
+}
+
+pc_disk = {"disk":{
+    "free disk space in gb": round(hdd.free / (10**9), 2),
+    "total disk space in gb": round(hdd.total / (10**9), 2),
+    "used disk space in %": round((hdd.used/hdd.free) * 100, 2)
+    }
+}
+
+with open ("pc_ram_log.json", "a") as q:
+    json.dump(pc_ram, q, indent=4)
+
+with open("pc_disk_log.json", "a") as q2:
+    json.dump(pc_disk, q2, indent=4)
 
