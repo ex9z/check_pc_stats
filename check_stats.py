23510cf0d5a05f5aaa1afce352cae8052db12839
) <dansteel53@gmail.com>
2022-05-26 02:41:21 +0300

    Update check_stats.py

--- check_stats.py
+++ check_stats.py
@@ -13,8 +13,15 @@ hdd = psutil.disk_usage('/')
 print("vaba salvestusruumi:", round(hdd.free / (10**9), 2), "gb")
 print("kokku salvestusruumi:", round(hdd.total / (10**9), 2), "gb")
 
-warning = 20
+warning = 50
 
 if round((used_memory/total_memory) * 100, 2) > warning:
     print("ram usage is more than", warning, "%")
+else:
+    print("ram usage is fine")
+
+if round((hdd.used/hdd.free) * 100, 2) > warning:
+    print("disk usage is more than", warning, "%")
+else:
+    print("disk usage is fine")
 
